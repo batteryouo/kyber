1ed48fa242cfa7fd63683fdc2baadc409cbe95c3
batteryouo <oyinggaio@gmail.com>
2026-02-23 18:42:01 +0800

    add ip

--- ref/client_tcp.c
+++ ref/client_tcp.c
@@ -12,16 +12,34 @@
 
 #define PORT 8080
 
-int main() {
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("Usage: %s <server_ip>\n", argv[0]);
+        return -1;
+    }
+
+    const char *server_ip = argv[1];
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return -1;
+    }
+
     struct sockaddr_in serv_addr;
 
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
-    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+    if (inet_pton(AF_INET, server_ip, &serv_addr.sin_addr) <= 0) {
+        printf("Invalid IP address\n");
+        return -1;
+    }
 
-    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connect");
+        return -1;
+    }
+    
     uint8_t shared_secret[pqcrystals_kyber768_BYTES];
 
     if (client_handshake(sock, shared_secret) != 0) {
