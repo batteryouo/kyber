e644a9e6591e799d0d4d210ab1cf9c74f3d5dbe4
batteryouo <oyinggaio@gmail.com>
2026-02-26 12:54:35 +0800

    refactor

--- ref/secure_channel.c
+++ ref/secure_channel.c
@@ -5,17 +5,11 @@
 #include "crypto_utils.h"
 #include "net_utils.h"
 
-int secure_channel_init(secure_channel_t *ch,
-                        int sock,
-                        int is_server,
-                        const uint8_t *shared_secret){
+int secure_channel_init(secure_channel_t *ch, int sock, int is_server, const uint8_t *shared_secret){
     ch->sock = sock;
     ch->is_server = is_server;
 
-    derive_keys(shared_secret,
-                ch->client_key,
-                ch->server_key,
-                ch->base_nonce);
+    derive_keys(shared_secret, ch->client_key, ch->server_key, ch->base_nonce);
 
     ch->send_counter = 1;
     ch->recv_counter = 1;
@@ -23,17 +17,14 @@ int secure_channel_init(secure_channel_t *ch,
     return 0;
 }
 
-int secure_send(secure_channel_t *ch,
-                const uint8_t *data,
-                uint32_t len){
+int secure_send(secure_channel_t *ch, const uint8_t *data, uint32_t len){
     uint8_t nonce[12];
     uint8_t ciphertext[2048];
     uint8_t tag[16];
 
     make_nonce(nonce, ch->base_nonce, ch->send_counter);
 
-    const uint8_t *key =
-        ch->is_server ? ch->server_key : ch->client_key;
+    const uint8_t *key = ch->is_server ? ch->server_key : ch->client_key;
 
     aes_gcm_encrypt(key, nonce, data, len, ciphertext, tag);
 
@@ -46,9 +37,7 @@ int secure_send(secure_channel_t *ch,
     return 0;
 }
 
-int secure_recv(secure_channel_t *ch,
-                uint8_t *buffer,
-                uint32_t *out_len){
+int secure_recv(secure_channel_t *ch, uint8_t *buffer, uint32_t *out_len){
     uint8_t nonce[12];
     uint8_t ciphertext[2048];
     uint8_t tag[16];
@@ -68,12 +57,7 @@ int secure_recv(secure_channel_t *ch,
     const uint8_t *key =
         ch->is_server ? ch->client_key : ch->server_key;
 
-    if (aes_gcm_decrypt(key,
-                        nonce,
-                        ciphertext,
-                        len,
-                        tag,
-                        buffer) != 0)
+    if (aes_gcm_decrypt(key, nonce, ciphertext, len, tag, buffer) != 0)
         return -1;
 
     ch->recv_counter++;
